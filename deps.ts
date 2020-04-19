9147eb48c7105455b60a1ba9580dc29980d878d6
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-19 01:15:23 -0400

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,12 @@
+export {
+  chooseOption,
+  determineIfPhaseIsLegal,
+  isLegalOption,
+  startGame,
+  getOptions,
+  LobbyPlayer,
+  Option,
+  Phase,
+  PlayerPosition,
+} from "https://raw.githubusercontent.com/alexjamesmalcolm/deno-bid-euchre-engine/master/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v0.41.0/mod.ts";
---
+++ main.ts
@@ -0,0 +1,67 @@
+import {
+  Drash,
+  startGame,
+  getOptions,
+  chooseOption,
+  determineIfPhaseIsLegal,
+  isLegalOption,
+  LobbyPlayer,
+  Option,
+  Phase,
+  PlayerPosition,
+} from "./deps.ts";
+
+class ChooseOptionResource extends Drash.Http.Resource {
+  static paths = ["/choose-option"];
+  public PUT() {
+    const phase: Phase = this.request.getBodyParam("phase");
+    const [isPhaseLegal, error] = determineIfPhaseIsLegal(phase);
+    if (!isPhaseLegal) return { error };
+    const option: Option = this.request.getBodyParam("option");
+    const currentPlayer: PlayerPosition = this.request.getBodyParam(
+      "currentPlayer"
+    );
+    if (!isLegalOption(option, phase, currentPlayer)) {
+      return { error: "Not a legal option" };
+    }
+    this.response.body = { phase: chooseOption(option, phase, currentPlayer) };
+    return this.response;
+  }
+}
+
+class GetOptionsResource extends Drash.Http.Resource {
+  static paths = ["/get-options"];
+  public PUT() {
+    const phase: Phase = this.request.getBodyParam("phase");
+    const currentPlayer: PlayerPosition = this.request.getBodyParam(
+      "currentPlayer"
+    );
+    this.response.body = { options: getOptions(phase, currentPlayer) };
+    return this.response;
+  }
+}
+
+class StartGameResource extends Drash.Http.Resource {
+  static paths = ["/start-game"];
+  public PUT() {
+    const players: [
+      LobbyPlayer,
+      LobbyPlayer,
+      LobbyPlayer,
+      LobbyPlayer
+    ] = this.request.getBodyParam("players");
+    const result = startGame(players);
+    this.response.body = Array.isArray(result)
+      ? { error: result[1] }
+      : { response: result };
+    return this.response;
+  }
+}
+
+const server = new Drash.Http.Server({
+  address: "localhost:3000",
+  response_output: "application/json",
+  resources: [ChooseOptionResource, GetOptionsResource, StartGameResource],
+});
+
+server.run();
